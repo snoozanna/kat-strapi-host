127fce7b615cfc06d0950cc05278e1ed841e50b9
suzannahurst <hurstsuzanna@gmail.com>
2023-04-13 15:29:53 +0100

    adjust db

--- config/database.js
+++ config/database.js
@@ -1,13 +1,28 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'mysql',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 3306),
+//       database: env('DATABASE_NAME', 'testing'),
+//       user: env('DATABASE_USERNAME', 'root'),
+//       password: env('DATABASE_PASSWORD', ''),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'testing'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "91.238.161.176"),
+      port: env.int("DATABASE_PORT", 3306),
+      // its always this port
+      database: env("DATABASE_NAME", "snoozann_kat-strapi-host-db"),
+      user: env("DATABASE_USERNAME", "snoozann_user1"),
+      password: env("DATABASE_PASSWORD", "Testing9"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
