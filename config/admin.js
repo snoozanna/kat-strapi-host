2b9b12935fef064f868296a85b93e716ea446bc8
suzannahurst <hurstsuzanna@gmail.com>
2023-04-23 23:17:42 +0100

    added url to admin

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
+  url: "https://strapi.snoozanna.co.uk/admin",
 });
